a518864454d9cbddf8589c208fd539f95d9b4fca
MahammadA <mahammada@risk.az>
2024-10-25 00:19:30 +0400

    os basics

--- AdvancedTopics/Docker_Containers/Program.cs
+++ AdvancedTopics/Docker_Containers/Program.cs
@@ -1,7 +1,39 @@
-﻿internal class Program
+﻿namespace Docker_Containers
 {
-    private static void Main(string[] args)
+    internal class Program
     {
+        private static void Main(string[] args)
+        {
+            /* Operating System Basics
+             
+            An operating system (OS) is software that manages hardware resources and 
+            provides services to other software (applications). It's made up of different layers:
 
+            1. User Space (Applicatin Layer)
+            This is the layer where user applications run. 
+            Programs like browsers, editors, etc., operate in this space.
+
+            Applications in the application layer make requests to the OS 
+            through system calls to interact with hardware (e.g., file reading, memory allocation).
+
+            2. Kernel Space
+            The kernel is the core part of the OS and operates at a low level, 
+            directly interacting with the hardware.
+            
+            The kernel manages crucial tasks like memory, CPU scheduling, and device communication.
+            It's responsible for resource management (CPU, memory, disk, I/O devices) and 
+            ensures applications use hardware without conflicting.
+
+            3. Hardware
+            This includes all physical devices, like the CPU, RAM, storage, network interfaces, and peripherals.
+            In simple terms, applications can't access hardware directly. 
+            They request services (like writing to disk or sending data) from the kernel, which controls the hardware.
+
+
+            -----What is System Calls?
+
+            */
+
+        }
     }
 }
