ad6cd23a71a432c2db6262ae8f7711cb25e7ffe9
MahammadA <mahammada@risk.az>
2025-03-16 18:38:12 +0400

    Dunder Methods

--- main.py
+++ main.py
@@ -1194,3 +1194,110 @@ my_dict = {
 
 # These are special methods that Python automatically calls under specific circumstances,
 # and they allow you to define the behavior of your objects in certain situations.
+
+
+# 1. __init__ (Constructor)
+# Purpose: Initializes an object's state when it is created. It is called automatically when an object is instantiated.
+
+
+# class Dog:
+#     def __init__(self, name, breed):
+#         self.name = name
+#         self.breed = breed
+
+
+# dog = Dog("Rex", "German Shepherd")
+# print(dog.name)  # Output: Rex
+# print(dog.breed)  # Output: German Shepherd
+
+
+# 2. __str__ (String Representation)
+# Purpose: This method defines how an object should be represented as a string when it is printed.
+# It is used by the print() function or str() on an object.
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def __str__(self):
+#         return f"Person(Name: {self.name}, Age: {self.age})"
+
+
+# person = Person("mahammad", 21)
+# print(person)  # Output: Person(Name: mahammad, Age: 21)
+
+
+# 3. __len__ (Length of Object)
+# Purpose: Used by the len() function to get the length of the object.
+
+
+# class MyList:
+#     def __init__(self, data):
+#         self.data = data
+
+#     def __len__(self):
+#         return len(self.data)
+
+
+# my_list = MyList([1, 2, 3, 4])
+# print(len(my_list))  # Output: 4
+
+# for num in my_list.data:
+#     print(num)
+
+
+# 4. __add__ (Addition)
+# Purpose: This method is called when the + operator is used between objects.
+# It allows you to define custom addition behavior for your objects.
+
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def __add__(self, other):
+#         return Point(self.x + other.x, self.y + other.y)
+
+
+# point1 = Point(2, 3)
+# point2 = Point(1, 1)
+# point3 = point1 + point2  # Calls __add__
+# print(point3.x, point3.y)  # Output: 3 4
+
+
+# 5. __getitem__ (Indexing)
+# Purpose: This method allows you to define custom behavior for index-based access,
+# similar to how you access elements in a list.
+
+
+# class MyList:
+#     def __init__(self, data):
+#         self.data = data
+
+#     def __getitem__(self, index):
+#         return self.data[index]
+
+
+# my_list = MyList([10, 20, 30])
+# print(my_list[1])  # Output: 20
+
+
+# 6. __setitem__ (Index Assignment)
+# Purpose: This method allows you to define custom behavior for assignment to indexed elements,
+# such as obj[index] = value.
+
+
+# class MyList:
+#     def __init__(self, data):
+#         self.data = data
+
+#     def __setitem__(self, index, value):
+#         self.data[index] = value
+
+
+# my_list = MyList([10, 20, 30])
+# my_list[1] = 50
+# print(my_list.data)  # Output: [10, 50, 30]
